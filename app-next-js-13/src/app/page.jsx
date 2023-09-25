0402df18efe68b6aa394e849c7a401d24944fc47
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-09-25 17:16:41 -0300

    First Commit
    Create project with next.js v.13

---
+++ app-next-js-13/src/app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 text-center max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+        APP with Next.js v.13
+      </div>
+    </main>
+  )
+}
